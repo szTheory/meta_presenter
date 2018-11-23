3292b4d6d6a8592412aecd7b5bba57be3cb188cf
szTheory <szTheory@users.noreply.github.com>
2018-11-23 12:12:42 -0500

    clean up spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,31 @@
+# spec coverage
 require 'coveralls'
 Coveralls.wear!
 
-require "meta_presenter"
-Dir["#{__dir__}/support/app/**/*.rb"].each {|file| require file }
+# debug tool
 require "pry"
 
-unless defined?(Rails)
+# main lib module
+require "meta_presenter"
+
+# support files
+Dir["#{__dir__}/support/*.rb"].each {|file| require file }
+
+# Simulate the Rails env.
+# Instead of loading the Rails environment, we stub the bare amount
+# and load from spec/support/app as if it were a Rails app
+begin
+  # mount app dir tree
+  Dir["#{__dir__}/support/app/**/*.rb"].each {|file| require file }
+
+  # mock Rails
   class Rails
   end
-end
 
-RSpec.configure do |config|
-  config.before do
-    allow(Rails).to receive(:root).and_return(File.join(__dir__, 'support'))
+  # stub Rails.root
+  RSpec.configure do |config|
+    config.before do
+      allow(Rails).to receive(:root).and_return(File.join(__dir__, 'support'))
+    end
   end
 end
