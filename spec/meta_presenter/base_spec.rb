f4c190936f1dc3ffbb23aed4dcc612947cbf4bf0
szTheory <szTheory@users.noreply.github.com>
2019-01-15 21:52:27 -0500

    specs for MetaPresenter::Base#inspect

---
+++ spec/meta_presenter/base_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe MetaPresenter::Base do
+
+  describe '#inspect' do
+    subject { presenter.inspect }
+
+    let(:controller_class) { ApplicationController }
+    let(:controller) { controller_class.new }
+    let(:action_name) { 'logs' }
+    let(:presenter) { controller.view_context.presenter }
+    
+    before do
+      allow(controller).to receive(:action_name).and_return(action_name)
+    end
+
+    it { is_expected.to eql('#<ApplicationPresenter>') }
+  end
+end
--- spec/meta_presenter/builder_spec.rb
+++ spec/meta_presenter/builder_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-# require 'ostruct'
 
 describe MetaPresenter::Builder do
   let(:controller_class) { ApplicationController }
